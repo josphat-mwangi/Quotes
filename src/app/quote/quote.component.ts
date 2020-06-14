2d09c3d8f6fd6b6ed4f82e15bdf6b8bccf1295c1
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-06-14 07:09:19 +0300

    Generate component quote-detail

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.css'],
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[] = [
+    { Author: '', name: '' },
+    { Author: '', name: '' },
+    { Author: '', name: '' },
+    { Author: '', name: '' },
+    { Author: '', name: '' },
+    { Author: '', name: '' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
